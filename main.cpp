28bbc16a23a7216a98f44e30647a08b8f5e9c391
slythe6 <123780992+slythe6@users.noreply.github.com>
2023-02-23 14:10:26 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,96 @@
+//CS311 Yoshii FALL21
+//INSTRUCTION:
+//Look for ** to complete this program (Use control-S)
+//The output should match my hw1stackDemo.out 
+// NEVER delete my comments !!!
+//=========================================================
+//HW#: HW1P1 stack application (post-fix evaluation)
+//Your name: Simon Hausmaninger
+//Complier:  g++
+//File type: client program client1.cpp
+//===========================================================
+
+using namespace std;
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include "stack.h"
+
+//Purpose of the program: Communicate with user to solve postfix problems
+//Algorithm: ** Receive post fix expresssion
+// while char is of type int, push to stack
+//when operand is recieved, pop
+int main()
+{
+  stack postfixstack;  // integer stack
+  string expression;   // user entered expression
+  
+  cout << "type a postfix expression: " ;
+  cin >> expression;
+  
+  int i = 0;  // character position within expression
+  char item;  // one char out of the expression
+  
+  int box1;  // receive things from pop
+  int box2;  // receive things from pop
+  int result;
+  while (expression[i] != '\0') // til the end of the expression
+    {
+     try
+       {
+	 item = expression[i];  // current char
+	 // ** do all the steps in the algorithm given in Notes-2A
+	 if(item > 47 && item < 58){
+     int num;
+     num = expression[i] - '0';
+     postfixstack.push(num);
+     postfixstack.displayAll();
+   } else if((item == '+') || (item == '-') || (item == '*')) {
+     postfixstack.pop(box1);
+     postfixstack.pop(box2);
+    
+     if(item == '+'){
+     result = box2 + box1; 
+     postfixstack.push(result);
+     }
+     else if(item == '-'){
+     result = box2 - box1;
+     postfixstack.push(result);
+     }
+     else if(item == '*'){
+     result = box2 * box1;
+     postfixstack.push(result);
+     }
+
+    
+    }
+     else{
+      cout<< "Invalid element" << endl;
+      exit(1);
+   }
+   
+       } // this closes try
+      // Catch exceptions and report problems and quit the program now (exit(1)). 
+      // Error messages describe what is wrong with the expression.
+      catch (stack::Overflow) 
+	{cerr << "ERROR: StackOverflow - Expression has too many operands" << endl; }
+      catch (stack::Underflow)
+	{cerr << "ERROR: StackUnderflow - Expression has too few operands" << endl; }
+      catch (char const* errormsg ) // for invalid item case
+	{cerr << "Error: invalid expression" << endl; }
+      // go back to the loop after incrementing i
+      i+=1;
+    }// end of while
+  
+ // After the loop successfully completes: 
+ // Pop the result and show it.
+   postfixstack.pop(result);
+   cout << "Result = " << result << endl;
+ // If anything is left on the stack, an incomplete expression 
+ // was found so also inform the user.
+   if(!(postfixstack.isEmpty()))
+   cout << "Incomplete Expression" << endl;
+
+   postfixstack.clearIt();
+
+}// end of the program
---
+++ stack.h
@@ -0,0 +1,78 @@
+//CS311 Yoshii 
+//INSTRUCTION:
+//Stack class header file template based on Notes-1
+//You must complete the ** parts and then complete stack.cpp
+//EMACS HINT:
+//  control-S does searches
+//  Tab on each line will indent perfectly for C++
+
+// =======================================================
+// HW#: HW1P1 stack
+// Your name: Simon Hausmaninger
+// Compiler:  g++ 
+// File type: headher file stack.h
+//=======================================================
+
+//----- Globally setting up the aliases ----------------
+
+const int MAX = 10;   // The MAX number of elements for the stack
+                      // MAX is unknown to the client
+
+typedef int el_t;      // the el_t type is ** for now
+                      // el_t is unknown to the client
+//-------------------------------------------------------
+
+
+class stack
+{ 
+  
+ private: // to be hidden from the client
+  
+   el_t     el[MAX];       // el is  an array of el_t's
+   int      top;           // top is index to the top of stack
+
+ public:  // available to the client
+  
+  // Add exception handling classes here  
+   class Overflow{};
+   class Underflow{};
+   class errormsg{};
+  // prototypes to be used by the client
+  // Note that no parameter variables are given
+
+  stack();   // constructor to create an object
+  ~stack();  // destructor  to destroy an object
+  
+  // PURPOSE: if not full, enters an element at the top;
+  //          otherwise throws an exception - Overflow
+  // PARAMETER: pass the element to be pushed
+  void push(el_t);
+  
+  // PURPOSE: if not empty, removes and gives back the top element;
+  //          otherwise throws an exception - Underflow
+  // PARAMETER: provide variable to receive the popped element (by ref)
+  void pop(el_t&);
+  
+  // PURPOSE: if not empty, gives the top element without removing it;
+  //          otherwise, throws an exception - Underflow
+  // PARAMETER: Returns top element of the stack
+  void topElem(el_t&);
+  
+  // ** Must add good comments for each function - See Notes1B
+
+  //PURPOSE: returns true if stack is empty, false if stack is not empty
+  bool isEmpty();
+
+  //PURPOSE: returns true if stack is full, false if not full
+  bool isFull();
+
+  //PURPOSE: displays all current elements of stack
+  void displayAll();
+
+  //PURPOSE: empties stack by cycling through and popping each element
+  void clearIt();
+  
+};  
+
+// Note: semicolon is needed at the end of the header file
+
